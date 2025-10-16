1f4cbea65b90cc4e4405e604b7ed3f6f7d78e02a
你滴琪BOY <1287918131@qq.com>
2025-10-17 06:57:12 +0800

    停止容器

--- backend/src/instances/docker.service.ts
+++ backend/src/instances/docker.service.ts
@@ -73,15 +73,12 @@ export class DockerService {
   async stopContainer(dockerId: string): Promise<void> {
     try {
       const container = this.docker.getContainer(dockerId);
-      const info = await container.inspect();
-      if (!info.State || info.State.Status === 'exited' || info.State.Status === 'dead' || info.State.Running === false) {
-        return;
-      }
       await container.stop();
     } catch (error) {
       if (error?.statusCode === 304) {
         return;
       }
+      console.log(`停止容器失败: ${error.message}`)
       throw new InternalServerErrorException(`停止容器失败: ${error.message}`);
     }
   }
